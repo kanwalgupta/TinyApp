32b699b61caf1a4d6ad1cef7fb03257b01be418d
kanwalgupta <kanwal.gupta1@gmail.com>
2018-08-08 19:16:05 +0000

    Code for deleting an url

--- express_server.js
+++ express_server.js
@@ -45,6 +45,11 @@ app.post("/urls", (req, res) => {
   console.log(urlDatabase);  // debug statement to see POST parameters
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
